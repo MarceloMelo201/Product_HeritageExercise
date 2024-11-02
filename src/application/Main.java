98e90c1c932d7e1d737c599f9c4283b2c2d98d44
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-02 18:51:32 -0300

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,65 @@
+package application;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        List<Product> list = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Product #" + (i+1) + " data:");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            String choice = sc.next();
+
+            System.out.print("Name: ");
+            String name = sc.next();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+
+            if(choice.equals("c")){
+                list.add(new Product(name, price));
+            }
+            else if (choice.equals("u")){
+
+                System.out.print("Manufacture date (DD/MM/YYYY): ");
+                Date manufactureDate = sdf.parse(sc.next());
+                list.add(new UsedProduct(name, price, manufactureDate));
+            }
+            else if(choice.equals("i")){
+
+                System.out.print("Custom Fee: ");
+                double customFee = sc.nextDouble();
+                list.add(new ImportedProduct(name, price, customFee));
+            }
+            else {
+
+                System.out.println("Enter a valid option!");
+            }
+        }
+
+        System.out.println();
+        System.out.println("PRICE TAGS:");
+
+        for(Product c: list){
+            System.out.println(c.priceTag());
+        }
+        sc.close();
 
     }
 }
--- src/entities/UsedProduct.java
+++ src/entities/UsedProduct.java
@@ -1,2 +1,41 @@
-package entities;public class UsedProduct {
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product{
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Date manufactureDate;
+
+    public UsedProduct() {
+    }
+
+    public UsedProduct(String name, Double price, Date manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(name)
+                .append(" (used)")
+                .append(" $ ")
+                .append(price)
+                .append(" (Manufacture date: ")
+                .append(sdf.format(manufactureDate))
+                .append(")");
+
+        return sb.toString();
+    }
 }
